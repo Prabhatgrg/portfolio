ff5186702f4d56938c56f02eecb6952473c43528
Prabhatgrg <prabhatgurung34@gmail.com>
2024-01-06 16:57:34 +0545

    Installed AOS library for scroll animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import NavBar from '@/Components/NavBar'
 
 export default function Home() {
   return (
     <main>
+      <NavBar />
       <h1>Test Page</h1>
     </main>
   )
