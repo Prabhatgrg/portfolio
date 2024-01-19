95356e1f2c6b0e68a212c91aff4cefe340be74f9
Prabhatgrg <prabhatgurung34@gmail.com>
2024-01-19 21:42:40 +0545

    Further work on sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,9 @@ export default function Home() {
   return (
     <>
       <SideBar />
-      <main className="bg-gray-400 h-screen flex justify-center ml-80">
+      <main className="bg-gray-500 h-screen flex justify-center ml-80">
         <div className="pt-12 flex justify-center items-center">
-          <h1 className="font-extrabold text-6xl">Prabhat Gurung</h1>
+          <h1 className="font-extrabold text-6xl text-slate-200">Prabhat Gurung</h1>
         </div>
       </main>
     </>
