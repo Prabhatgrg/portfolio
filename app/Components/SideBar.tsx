c78a1980c118ff9334226638ef6a88f1230c88ff
Prabhatgrg <prabhatgurung34@gmail.com>
2024-01-21 22:54:42 +0545

    Sidebar items addition

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -4,6 +4,13 @@ const SideBar = () => {
   return (
     <aside className="bg-[#111827] flex flex-col h-screen fixed w-1/4">
       <h1 className="text-white">This is a SideBar</h1>
+      <div className="lists text-white flex justify-center items-center mt-10">
+        <ul className="flex flex-col gap-10">
+          <li>Home</li>
+          <li>About</li>
+          <li>Projects</li>
+        </ul>
+      </div>
     </aside>
   );
 };
