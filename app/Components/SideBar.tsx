84f0ebebe2b9e7bbe290ce26419fc945e1af8a40
Prabhatgrg <prabhatgurung34@gmail.com>
2024-01-22 20:48:54 +0545

    Further Sidebar items layout management

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -3,8 +3,11 @@ import React from "react";
 
 const SideBar = () => {
   return (
-    <aside className="bg-[#111827] flex flex-col h-screen fixed w-1/4">
+    <aside className="bg-[#111827] flex flex-col h-screen fixed items-center w-1/4">
       <h1 className="text-white text-center mt-10">Prabhat Gurung</h1>
+      <div className="socials mt-6 text-white">
+        Social Media Icons Goes Here
+      </div>
       <div className="lists text-white flex ms-5 items-center mt-10">
         <ul className="flex flex-col gap-10">
           <Link href="/">
