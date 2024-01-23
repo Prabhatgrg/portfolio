18a3e02db8ed8e92494ee7dcee807f6c17455251
Prabhatgrg <prabhatgurung34@gmail.com>
2024-01-23 19:36:27 +0545

    Adding active class to the active links for highlighting active links

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -1,7 +1,12 @@
+'use client'
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const SideBar = () => {
+
+  const router = usePathname();
+
   return (
     <aside className="bg-[#111827] flex flex-col h-screen fixed items-center w-1/4">
       <h1 className="text-white text-center mt-10">Prabhat Gurung</h1>
@@ -10,13 +15,13 @@ const SideBar = () => {
       </div>
       <div className="lists text-white flex ms-5 items-center mt-10">
         <ul className="flex flex-col gap-10">
-          <Link href="/">
+          <Link className={router == "/" ? "active" : ""} href="/">
             <li>Home</li>
           </Link>
-          <Link href="/">
+          <Link className={router == "/about" ? "active" : ""} href="/about">
             <li>About</li>
           </Link>
-          <Link href="/">
+          <Link className={router == "/projects" ? "active" : ""} href="/projects">
             <li>Projects</li>
           </Link>
           <Link href="/">
