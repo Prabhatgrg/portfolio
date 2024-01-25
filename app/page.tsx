ab56dd4912713d80dc0990495b380c4955244ef2
Prabhatgrg <prabhatgurung34@gmail.com>
2024-01-25 20:56:04 +0545

    Added a background image for making the site a bit pretty

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
 import Image from "next/image";
 import SideBar from "@/Components/SideBar";
+import BgImage from "public/night-sky.jpg";
 
 export default function Home() {
   return (
     <>
       <SideBar />
-      <main className="bg-gray-500 h-screen flex justify-center ml-80">
+      {/* <main className="bg-gray-500 h-screen flex justify-center ml-80"> */}
+      <main className="h-screen flex justify-center ml-80">
         <div className="pt-12 flex justify-center items-center">
-          <h1 className="font-extrabold text-6xl text-slate-200">Prabhat Gurung</h1>
+          <h1 className="font-extrabold text-6xl text-slate-200">
+            <Image
+              src={BgImage}
+              fill
+              alt="Background Image"
+              style={{
+                objectFit: "cover",
+                zIndex: -1,
+              }}
+            />
+            Prabhat Gurung
+          </h1>
         </div>
       </main>
     </>
