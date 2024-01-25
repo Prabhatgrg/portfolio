7f9d3fa9bcdca87e1e2d2ba438d824cefbcd61fa
Prabhatgrg <prabhatgurung34@gmail.com>
2024-01-25 21:29:17 +0545

    Object fit not working as intended need to fix that later

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
 import Image from "next/image";
 import SideBar from "@/Components/SideBar";
 import BgImage from "public/night-sky.jpg";
+import Coding from "public/coding-laptop.jpg";
+import Programming from "public/programming-illustration.svg";
 
 export default function Home() {
   return (
     <>
       <SideBar />
       {/* <main className="bg-gray-500 h-screen flex justify-center ml-80"> */}
-      <main className="h-screen flex justify-center ml-80">
+      <main className="min-w-60 flex justify-center ml-80">
         <div className="pt-12 flex justify-center items-center">
           <h1 className="font-extrabold text-6xl text-slate-200">
             <Image
-              src={BgImage}
+              src={Coding}
               fill
+              className="h-screen"
               alt="Background Image"
               style={{
+                marginLeft: 200,
                 objectFit: "cover",
                 zIndex: -1,
               }}
