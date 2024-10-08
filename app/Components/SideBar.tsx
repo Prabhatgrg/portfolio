702a86f737e32cd34390435111d806c72330b2f3
prabhatgrg <prabhatgurung34@gmail.com>
2024-10-09 00:45:09 +0545

    feat: Changed Icon PlaceHolder

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -11,7 +11,7 @@ const SideBar = () => {
     <aside className="bg-[#111827] flex flex-col h-screen fixed items-center w-1/4">
       <h1 className="text-white text-center mt-10">Prabhat Gurung</h1>
       <div className="socials mt-6 text-white">
-        Social Media Icons Goes Here
+        Social Media Icons
       </div>
       <div className="lists text-white flex ms-5 items-center mt-10">
         <ul className="flex flex-col gap-10 w-full">
