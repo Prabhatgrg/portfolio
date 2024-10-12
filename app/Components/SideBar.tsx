5517410bc53150d98a2863197113f3f8f3d85d4a
prabhatgrg <prabhatgurung34@gmail.com>
2024-10-12 22:29:56 +0545

    feat: Added social icons with link

--- app/Components/SideBar.tsx
+++ app/Components/SideBar.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { FaGithub, FaFacebook, FaLinkedin } from "react-icons/fa";
 import React from "react";
 
 const SideBar = () => {
@@ -11,7 +12,18 @@ const SideBar = () => {
     <aside className="bg-[#111827] flex flex-col h-screen fixed items-center w-1/4">
       <h1 className="text-white text-center mt-10">Prabhat Gurung</h1>
       <div className="socials mt-6 text-white">
-        Social Media Icons
+        {/*Social Media Icons*/}
+          <div className="flex justify-center gap-5">
+              <Link href="https://github.com/Prabhatgrg" target="_blank">
+                  <FaGithub size={30} />
+              </Link>
+              <Link href="https://www.facebook.com/prabhat.gurung.180/" target="_blank">
+                  <FaFacebook size={30} />
+              </Link>
+              <Link href="https://www.linkedin.com/in/prabhat-g-661204109/" target="_blank">
+                  <FaLinkedin size={30} />
+              </Link>
+          </div>
       </div>
         <div className="lists text-white flex items-center mt-10 w-full">
             <ul className="flex flex-col items-center gap-10 w-full">
