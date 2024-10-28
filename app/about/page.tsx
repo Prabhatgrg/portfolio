954d61bddcd5e508bb77dae2c84fe4593b8ccf08
prabhatgrg <prabhatgurung34@gmail.com>
2024-10-28 22:06:59 +0545

    feat: About me page layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,14 +3,16 @@ import SideBar from "@/Components/SideBar";
 const About = () => {
   return (
       <>
-          <SideBar/>
-          <main className="flex justify-center bg-black h-screen items-center">
-              <div className="pt-12">
-                  <h1 className="font-extrabold text-6xl text-white">
-                      About Me
-                  </h1>
-              </div>
-          </main>
+          <div>
+              <SideBar/>
+              <aside className="w-full flex justify-center bg-black h-screen items-center">
+                  <div className="pt-12">
+                      <h1 className="font-extrabold text-6xl text-white">
+                          About Me
+                      </h1>
+                  </div>
+              </aside>
+          </div>
       </>
   );
 };
