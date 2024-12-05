b357b039071fa9ffc43c61cf1471b2d666a84ab3
prabhatgrg <prabhatgurung34@gmail.com>
2024-12-06 05:44:01 +0545

    feat: Keeping SideBar for now

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <>
         <div className="w-full flex">
-            {/*<div className="flex flex-col h-screen bg-[#111827] w-2/6 items-center">*/}
-            {/*    <SideBar />*/}
-            {/*</div>*/}
+            <div className="flex flex-col h-screen bg-[#111827] w-2/6 items-center">
+                <SideBar />
+            </div>
           <aside className="w-full flex justify-center">
             <div className="pt-12 flex justify-center items-center" data-aos="zoom-in">
               <h1 className="font-extrabold text-6xl text-slate-200">
