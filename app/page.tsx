07c61cab9295f962413b40778923f1c6628e93c9
prabhatgrg <prabhatgurung34@gmail.com>
2024-12-05 21:57:09 +0545

    feat: Changed Cover Image, Thinking of ditching the Sidebar looks uglyj

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
   return (
     <>
         <div className="w-full flex">
-            <div className="flex flex-col h-screen bg-[#111827] w-2/6 items-center">
-                <SideBar />
-            </div>
+            {/*<div className="flex flex-col h-screen bg-[#111827] w-2/6 items-center">*/}
+            {/*    <SideBar />*/}
+            {/*</div>*/}
           <aside className="w-full flex justify-center">
             <div className="pt-12 flex justify-center items-center" data-aos="zoom-in">
               <h1 className="font-extrabold text-6xl text-slate-200">
                 <Image
-                  src="/images/coding-laptop.jpg"
+                  src="/images/sagarmatha-banner.jpg"
                   fill
                   className="h-screen"
                   alt="Background Image"
